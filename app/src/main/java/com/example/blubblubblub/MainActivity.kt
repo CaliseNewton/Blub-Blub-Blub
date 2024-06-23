d3be694defc3ff6de868cd030838f27eee8d8872
CaliseNewton <newton@calise.dev>
2024-06-23 14:24:05 -0400

    Add babbling brook sounds and remove button on click.

--- app/src/main/java/com/example/blubblubblub/MainActivity.kt
+++ app/src/main/java/com/example/blubblubblub/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.blubblubblub
 
 import android.graphics.drawable.AnimationDrawable
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.ComponentActivity
@@ -9,6 +11,7 @@ import androidx.activity.ComponentActivity
 class MainActivity : ComponentActivity() {
 
     private lateinit var animationDrawable: AnimationDrawable
+    private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +21,12 @@ class MainActivity : ComponentActivity() {
         imageView.setBackgroundResource(R.drawable.frame_animation)
         animationDrawable = imageView.background as AnimationDrawable
 
+        mediaPlayer = MediaPlayer.create(this, R.raw.brook)
+
         val button: Button = findViewById(R.id.button)
         button.setOnClickListener {
+            (button.parent as? ViewGroup)?.removeView(button)
+            mediaPlayer.start()
             animationDrawable.start()
         }
     }
